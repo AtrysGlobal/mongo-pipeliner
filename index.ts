55e1e9198d394b079b21f1b6e382d1d1adc38c95
Eduardo Munoz <gstv.qnd@outlook.com>
2024-01-25 17:21:57 -0300

    🔖 Preparing release 0.1.0

---
+++ index.ts
@@ -0,0 +1,4 @@
+import { AggregationPipelineBuilder } from './lib/base/pipeline-builder';
+import { IDetailedAggregationPipelineBuilder, ILookupStageParams } from './lib/interface/pipeline-builder';
+
+export { AggregationPipelineBuilder, IDetailedAggregationPipelineBuilder, ILookupStageParams };
